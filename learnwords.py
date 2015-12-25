caf3d4c879307942e74431e499cb2c46df500dae
Denis <prohibiti@gmail.com>
2015-12-25 16:39:44 +0200

    add config function that reads settings from a file

--- learnwords.py
+++ learnwords.py
@@ -1,26 +1,34 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import tkinter as tk
 import sqlite3
 import random
 import re
+import configparser
+import tkinter as tk
+
 from crossword import Crossword
 
 
 class LearnWords():
     def __init__(self, db_name='engwords.db'):
         self.db_name = db_name
-        # [number of right answers, all answers count]
+        # [number of right answers, all answers that was counted]
         self.result = [0, 0]
-        # default background color
-        self.bg_color = '#F8DCB1'
-        # a little bit darker than bg_color
-        self.bg_darkest = '#E9B96E'
-        # color of elemnet if answer is right
-        self.color_ok = '#8AE234'
-        # color of elemnet if answer is wrong
-        self.color_no = '#F34949'
+        self.load_settings()
+
+    def load_settings(self):
+        '''Loads settings from file'''
+        self.config = configparser.ConfigParser()
+        try:
+            self.config.read('settings.ini')
+        except:
+            raise IOError('Can\'t read settings.ini')
+
+        #TODO add window with settings and write them to file
+        #        with open('settings.ini', 'w') as configfile:
+        #            self.config.write(configfile)
+        return
 
     def read_words(self, random=True, limit=10):
         # TODO decide where to place this code
@@ -54,34 +62,33 @@ class LearnWords():
         random.shuffle(res)
         return res
 
-    def init_main_frame(self, sx=0, sy=0, px=0, py=0):
+    def init_main_frame(self):
         '''Frame where all other frames placed.
         To clear this main_frame just call this function'''
         self.main_frame = tk.Frame(
             self.root,
-            width=sx,
-            height=sy,
-            bg=self.bg_color
+            width=self.config['gui']['min_w'],
+            height=self.config['gui']['min_h'],
+            bg=self.config['colors']['bg']
             )
-        self.main_frame.place(x=px, y=py)
+        self.main_frame.place(x=0, y=0)
         return
 
-    def init_gui(self, font_size=18):
-        '''Initializing GUI. Creating root window, setting window size,
+    def init_gui(self):
+        '''Initializing GUI. Creating root gui, setting gui size,
         creating menubar'''
         self.root = tk.Tk()
-        self.font_size = font_size
 
-        self.min_x = 1000
-        self.min_y = 700
-        max_x = 1300
-        max_y = 1200
-        self.root.minsize(self.min_x, self.min_y)
-        self.root.maxsize(max_x, max_y)
+        self.root.minsize(
+            self.config['gui']['min_w'],
+            self.config['gui']['min_h']
+            )
+        self.root.maxsize(
+            self.config['gui']['max_h'],
+            self.config['gui']['max_w']
+            )
 
-        self.init_main_frame(sx=self.min_x,
-                             sy=self.min_y
-                             )
+        self.init_main_frame()
 
         # menu
         menu_bar = tk.Menu(self.root)
@@ -129,9 +136,7 @@ class LearnWords():
     def guess_word_translation(self, word_count=10):
         '''You're given word-translation and you need to guess to what
         word from 2 given words it benongs'''
-        self.init_main_frame(sx=self.min_x,
-                             sy=self.min_y
-                             )
+        self.init_main_frame()
         self.read_words(random=True, limit=word_count)
 
         self.root.title('Guess word translation')
@@ -143,7 +148,7 @@ class LearnWords():
                          )
         frame.place(x=100, y=100)
         word_lbl = tk.Label(frame,
-                            font=self.font_size,
+                            font=self.config['gui']['font_size'],
                             text='Press enter to start',
                             width=20,
                             wraplength=150,
@@ -151,17 +156,17 @@ class LearnWords():
                             )
         result_lbl = tk.Label(frame,
                               text='',
-                              font=self.font_size,
+                              font=self.config['gui']['font_size'],
                               )
         left_lbl = tk.Label(frame,
                             text='',
-                            font=self.font_size,
+                            font=self.config['gui']['font_size'],
                             width=20,
                             wraplength=150,
                             )
         right_lbl = tk.Label(frame,
                              text='',
-                             font=self.font_size,
+                             font=self.config['gui']['font_size'],
                              width=20,
                              wraplength=150,
                              )
@@ -206,7 +211,7 @@ class LearnWords():
             self.pos += 1
             if self.pos >= word_count:
                 frame.destroy()
-                # send result to main window
+                # send result to main gui
                 self.show_result()
                 return
             fill_lbl()
@@ -221,7 +226,7 @@ class LearnWords():
     def spell_word(self, word_count=10):
         '''You're given a word translation. You need to spell word from
         memory'''
-        self.init_main_frame(sx=self.min_x, sy=self.min_y)
+        self.init_main_frame()
         self.read_words(word_count)
         self.root.title('Spell words')
         frame = tk.Frame(self.main_frame,
@@ -235,11 +240,11 @@ class LearnWords():
 
         # label with result
         result_lbl = tk.Label(frame,
-                              font=self.font_size,
+                              font=self.config['gui']['font_size'],
                               text='Write your answer below!')
         result_lbl.pack()
         answer_ent = tk.Entry(frame,
-                              font=self.font_size,
+                              font=self.config['gui']['font_size'],
                               width=50)
         answer_ent.focus()
         answer_ent.pack()
@@ -261,13 +266,13 @@ class LearnWords():
             self.pos += 1
             if self.pos >= word_count:
                 frame.destroy()
-                # send result to main window
+                # send result to main gui
                 self.show_result()
                 return
             word_lbl.config(text=self.words[self.pos][1])
 
         word_lbl = tk.Label(frame,
-                            font=self.font_size,
+                            font=self.config['gui']['font_size'],
                             text=self.words[self.pos][1])
         word_lbl.pack()
         answer_ent.bind('<Return>', lambda e: check_answer())
@@ -280,20 +285,18 @@ class LearnWords():
 
     def word_translation(self, word_count=10, display=0):
         # TODO add informative docstring
-        self.init_main_frame(sx=self.min_x,
-                             sy=self.min_y
-                             )
+        self.init_main_frame()
         self.read_words(random=True, limit=word_count)
         self.pos = 0
 
         if display == 0:
             word_guess = 1
-            window_title = 'Translation-word'
+            gui_title = 'Translation-word'
         else:
             word_guess = 0
-            window_title = 'Word-translation'
+            gui_title = 'Word-translation'
 
-        self.root.title(window_title)
+        self.root.title(gui_title)
 
         frame = tk.Frame(self.main_frame,
                          width=700,
@@ -304,18 +307,21 @@ class LearnWords():
         frame.place(x=0, y=0)
 
         word_lbl = tk.Label(frame,
-                            font=self.font_size,
+                            font=self.config['gui']['font_size'],
                             text='Press enter to start',
                             width=20,
                             wraplength=150,
                             justify=tk.CENTER
                             )
         result_lbl = tk.Label(frame,
-                              font=self.font_size)
+                              font=self.config['gui']['font_size'])
         str_lbls = [tk.StringVar() for _ in range(4)]
-        lbls = [tk.Label(frame,
-                         textvariable=str_lbls[i],
-                         font=self.font_size) for i in range(4)]
+
+        # labels with answers
+        lbls = [tk.Label(
+            frame,
+            textvariable=str_lbls[i],
+            font=self.config['gui']['font_size']) for i in range(4)]
 
         def fill_lbl():
             word = self.words[self.pos]
@@ -348,7 +354,7 @@ class LearnWords():
             self.pos += 1
             if self.pos >= word_count:
                 frame.destroy()
-                # send result to main window
+                # send result to main gui
                 self.show_result()
                 return
             fill_lbl()
@@ -373,10 +379,10 @@ class LearnWords():
         def create_cell(row, column, char):
             field[row][column] = crossword_cell(
                 frame,
-                font=self.font_size,
+                font=self.config['gui']['font_size'],
                 width=2,
                 justify=tk.CENTER,
-                bg=self.bg_darkest,
+                bg=self.config['colors']['bg_darker'],
                 def_char=char
                 )
             field[row][column].grid(row=row, column=column)
@@ -457,6 +463,8 @@ class LearnWords():
             # mouse click
             elif e.num == 1:
 #TODO highlight word translation
+# I need to find to what word relate current cell
+# but how?
                 if field[row + 1][column] or field[row - 1][column]:
                     self.move = 1
                 else:
@@ -470,31 +478,29 @@ class LearnWords():
                         continue
                     answer = cell.get()
                     if cell.def_char == answer:
-                        cell.configure(bg=self.color_ok)
+                        cell.configure(bg=self.config['colors']['right'])
                     elif answer != '':
-                        cell.configure(bg=self.color_no)
+                        cell.configure(bg=self.config['colors']['wrong'])
             return
 
         def reset_cells_bg():
             for row in field:
                 for cell in row:
                     if cell:
-                        cell.configure(bg=self.bg_darkest)
+                        cell.configure(bg=self.config['colors']['bg_darker'])
             return
 
         #TODO add functionality
         def check_answer():
             return
 
-        self.init_main_frame(sx=self.min_x,
-                             sy=self.min_y
-                             )
+        self.init_main_frame()
         self.read_words(random=True, limit=word_count)
         frame = tk.Frame(
             self.main_frame,
             width=1000,
             height=700,
-            bg=self.bg_color
+            bg=self.config['colors']['bg']
             )
 
         #TODO SET PROPER SIZE
@@ -509,7 +515,7 @@ class LearnWords():
         crossword_items = mc.generate_crossword()
 
         # view translations
-        transl_frm = tk.Frame(self.main_frame, bg=self.bg_color)
+        transl_frm = tk.Frame(self.main_frame, bg=self.config['colors']['bg'])
         transl_frm.place(x=0, y=0)
         transl_lbls = {}
 
@@ -566,6 +572,7 @@ class LearnWords():
         return
 
     def show_result(self):
+        self.init_main_frame()
         self.root.title('Result')
         msg = str(self.result[0]) + ' from ' + str(self.result[1])
         tk.Label(self.main_frame, text=msg).pack()
@@ -573,21 +580,21 @@ class LearnWords():
 
     def show_help(self):
         # TODO add more info
-        help_window = tk.Toplevel()
+        help_gui = tk.Toplevel()
         help_msg = 'Use arrow keys to answer in "Guess word"\n'
         help_msg += 'Use enter to answer in "Spell word"\n'
         help_msg += 'Use Control+q to quit the programm.\n'
-        tk.Label(help_window, text=help_msg).pack()
-        help_window.bind('<Control-q>', lambda e: help_window.destroy())
-        help_window.bind('<Control-Q>', lambda e: help_window.destroy())
+        tk.Label(help_gui, text=help_msg).pack()
+        help_gui.bind('<Control-q>', lambda e: help_gui.destroy())
+        help_gui.bind('<Control-Q>', lambda e: help_gui.destroy())
         return
 
     def show_about(self):
         # TODO add more info
-        about_window = tk.Toplevel()
-        tk.Label(about_window, text='This program is in developmnet').pack()
-        about_window.bind('<Control-q>', lambda e: about_window.destroy())
-        about_window.bind('<Control-Q>', lambda e: about_window.destroy())
+        about_gui = tk.Toplevel()
+        tk.Label(about_gui, text='This program is in developmnet').pack()
+        about_gui.bind('<Control-q>', lambda e: about_gui.destroy())
+        about_gui.bind('<Control-Q>', lambda e: about_gui.destroy())
         return
 
 
